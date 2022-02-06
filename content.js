62c196a1f09b5a5c0fc6abdc82be68449681110c
angelina-tsuboi <angelina.t1832@gmail.com>
2022-02-06 00:56:44 -0800

    feat: add dev tools func code

--- content.js
+++ content.js
@@ -1,3 +1,11 @@
-window.addEventListener('load', (event) => {
-    console.log('The page has fully loaded');
-});
+// window.addEventListener('load', (event) => {
+//     console.log('The page has fully loaded');
+//     browser.devtools.panels.create(
+//         "Media View",                      // title
+//         "icon.png",                // icon
+//         "devtools.html"      // content
+//       ).then((newPanel) => {
+//         newPanel.onShown.addListener(initialisePanel);
+//         newPanel.onHidden.addListener(unInitialisePanel);
+//       });
+// });
